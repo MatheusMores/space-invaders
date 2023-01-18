12c0dca58dcdc9fb78eb4f3bd2f201620bb265eb
MatheusMores <101605650+MatheusMores@users.noreply.github.com>
2023-01-18 09:16:03 -0300

    enemys

--- main.py
+++ main.py
@@ -4,24 +4,35 @@ import WConio2
 from colorit import *
 
 from modules.player import Player
+from modules.enemy import Enemy
 from modules.printTela import printTela
 
 player_x = 30
 player_y = 40
 
 def iniciarJogo():
-    cont = 0
     init_colorit()
     os.system('cls')
     cursor.hide()
 
     player1 = Player(player_x, player_y)
+    enemys = []
+    enemy1 = Enemy(20, 10)
+    enemy2 = Enemy(40, 10)
 
+    enemys.append(enemy1)
+    enemys.append(enemy2)
 
     while True:
-        printTela()
 
-        player1.shoot()
+        printTela()
+        
+        enemy1.printEnemy()
+
+        enemy2.printEnemy()
+
+
+        print(player1.shoot(enemys))
 
         player1.printPlayer()
 
